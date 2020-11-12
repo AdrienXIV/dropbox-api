26cf2514544438bbabecf900676553a0a036dbc9
adrien <adri_00@hotmail.fr>
2020-11-12 16:21:00 +0100

    🐛 FIX: correction bug scope mail.js

--- utils/mail.js
+++ utils/mail.js
@@ -2,23 +2,24 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 
+// Create a SMTP transport object
+const transport = nodemailer.createTransport({
+  service: 'Gmail',
+  host: ' smtp.gmail.com',
+  port: 465,
+  secure: true, // upgrade later with STARTTLS  host: "smtp.gmail.com",
+  tls: {
+    ciphers: 'SSLv3',
+  },
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
 exports.nodeMailerConnection = () => {
   try {
-    // Create a SMTP transport object
-    const transport = nodemailer.createTransport({
-      service: 'Gmail',
-      host: ' smtp.gmail.com',
-      port: 465,
-      secure: true, // upgrade later with STARTTLS  host: "smtp.gmail.com",
-      tls: {
-        ciphers: 'SSLv3',
-      },
-      requireTLS: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
     // verify connection configuration
     transport.verify(error => {
       if (error) throw error;
@@ -37,6 +38,29 @@ exports.sendMailRegister = email => {
     from: `Groupe 6 - IMIE <${process.env.EMAIL_USER}>`, // sender address
     to: email, // list of receivers
     subject: 'Inscription réussi', // Subject line
+    text, // plain text body
+    html, // html body
+  };
+
+  transport.sendMail(message, error => {
+    if (error) {
+      console.log('Error mail');
+      console.log(error.message);
+      return;
+    }
+    // if you don't want to use this transport object anymore, uncomment following line
+    transport.close(); // close the connection pool
+  });
+};
+
+exports.sendMailForgotPassword = email => {
+  const link = '';
+  const text = 'Lien pour réinitialiser le mot de passe :';
+  const html = `<p>${text} <a>${link}</a></p>`;
+  const message = {
+    from: `Groupe 6 - IMIE <${process.env.EMAIL_USER}>`, // sender address
+    to: email, // list of receivers
+    subject: 'Réinitialisation mot de passe', // Subject line
     text, // plain text body
     html, // html body
   };
