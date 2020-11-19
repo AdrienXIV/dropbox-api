3789c965efc2570663260015f5066019f850195f
adrien <adri_00@hotmail.fr>
2020-11-19 14:59:57 +0100

    📦 NEW: paquet helmet en middleware

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 // cache
 const NodeCache = require('node-cache');
@@ -30,6 +31,7 @@ app.use(
 );
 //add other middleware
 app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
