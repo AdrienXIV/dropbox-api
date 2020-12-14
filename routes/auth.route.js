4e8d7ccba999f00305acc3f5dbfacac63963021f
gaye <gayegayemboup@gmail.com>
2020-12-14 15:25:59 +0100

    modification auth route et main

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,12 +4,21 @@ const usersController = require('../controllers/user.controller');
 
 //routes
 const auth = express.Router();
-//users routes
+
+/**
+ * GET
+ */
+
+auth.get('/getprofil', usersController.getprofil);
+/**
+ * POST
+ */
 auth.post('/register', usersController.register);
 auth.post('/login', usersController.login);
-auth.post('/editprofil', usersController.editprofil);
-auth.get('/getprofil', usersController.getprofil);
-auth.put('/editprofil', usersController.editprofil);
 
+/**
+ * PUT
+ */
+auth.put('/editprofil', usersController.editprofil);
 
 module.exports = auth;
