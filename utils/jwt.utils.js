b8555387c2a3b65873e1b6007c7e6bf9ff78f8a1
gaye <gayegayemboup@gmail.com>
2020-12-14 15:23:14 +0100

    recupération ino token

--- utils/jwt.utils.js
+++ utils/jwt.utils.js
@@ -13,18 +13,11 @@ exports.generateTokenForUser = user => {
     },
   );
 };
-parseAuthorization = (authorization) => {
-  return (authorization != undefined) ? authorization.replace('Bearer ','') : null;
-}
-exports.getUserEmail = authorization => {
-  var userEmail = '';
-  var verifyToken = parseAuthorization(authorization);
-  if(verifyToken != null){
-    try{
-      const jwtToken =jwt.verify(verifyToken, process.env.JWT_SIGN_SECRET);
-      if(jwtToken != null)
-        userEmail = jwtToken.email;
-    }catch(err) {}
+
+exports.getToken = token => {
+  try {
+    return jwt.verify(token.split('Baerer ')[1], process.env.JWT_SIGN_SECRET);
+  } catch (error) {
+    return false;
   }
-  return userEmail;
-}
+};
